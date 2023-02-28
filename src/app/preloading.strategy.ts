ac83bc34063f9d95efbdd54378522d72e976bcb2
Assaad Awada <a_awada78@hotmail.com>
2023-02-28 22:56:45 +0100

    Added Injectable

--- src/app/preloading.strategy.ts
+++ src/app/preloading.strategy.ts
@@ -1,6 +1,8 @@
+import { Injectable } from '@angular/core';
 import { PreloadingStrategy, Route } from '@angular/router';
 import { Observable, of } from 'rxjs';
 
+@Injectable()
 export class CustomPreloadingStrategy implements PreloadingStrategy {
   preload(route: Route, fn: () => Observable<any>): Observable<any> {
     if (route.data && route.data.preloading) {
